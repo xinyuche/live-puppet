4dba8bc78864349c8738f7048750095dbb60c5b5
xinyuche <xinyuche@buffalo.edu>
2021-11-29 17:22:37 -0500

    tryout tcp

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net"
-	"os"
 )
 
 const (
@@ -16,18 +15,18 @@ func main() {
 	// Listen for incoming connections.
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		os.Exit(1)
+		log.Printf("Error listening: %s", err.Error())
+		return
 	}
 	// Close the listener when the application closes.
 	defer l.Close()
-	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	log.Printf("Listening on " + CONN_HOST + ":" + CONN_PORT)
 	for {
 		// Listen for an incoming connection.
 		conn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			os.Exit(1)
+			log.Printf("Error accepting: %s", err.Error())
+			return
 		}
 		// Handle connections in a new goroutine.
 		go handleRequest(conn)
@@ -41,7 +40,7 @@ func handleRequest(conn net.Conn) {
 	// Read the incoming connection into the buffer.
 	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		log.Printf("Error reading: %s", err.Error())
 	}
 	// Send a response back to person contacting us.
 	conn.Write([]byte("Message received."))
