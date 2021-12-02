9fbf97f630aec9965672038174100e67a4122010
xinyuche <xinyuche@buffalo.edu>
2021-12-02 10:14:37 -0500

    change host to 0.0.0.0

--- main.go
+++ main.go
@@ -1,71 +1,71 @@
 package main
 
-// import (
-// 	"log"
-// 	"net"
-// )
-
-// const (
-// 	CONN_HOST = "localhost"
-// 	CONN_PORT = "8180"
-// 	CONN_TYPE = "tcp"
-// )
-
-// func main() {
-// 	// Listen for incoming connections.
-// 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-// 	if err != nil {
-// 		log.Printf("Error listening: %s", err.Error())
-// 		return
-// 	}
-// 	// Close the listener when the application closes.
-// 	defer l.Close()
-// 	log.Printf("Listening on " + CONN_HOST + ":" + CONN_PORT)
-// 	for {
-// 		// Listen for an incoming connection.
-// 		conn, err := l.Accept()
-// 		if err != nil {
-// 			log.Printf("Error accepting: %s", err.Error())
-// 			return
-// 		}
-// 		// Handle connections in a new goroutine.
-// 		go handleRequest(conn)
-// 	}
-// }
-
-// // Handles incoming requests.
-// func handleRequest(conn net.Conn) {
-// 	// Make a buffer to hold incoming data.
-// 	buf := make([]byte, 1024)
-// 	// Read the incoming connection into the buffer.
-// 	_, err := conn.Read(buf)
-// 	log.Printf("received: %s", string(buf[:]))
-// 	if err != nil {
-// 		log.Printf("Error reading: %s", err.Error())
-// 	}
-// 	// Send a response back to person contacting us.
-// 	conn.Write([]byte("Message received."))
-// 	// Close the connection when you're done with it.
-// 	conn.Close()
-// }
-
 import (
-	"fmt"
 	"log"
-	"net/http"
+	"net"
 )
 
-// HelloServer responds to requests with the given URL path.
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, you requested: %s", r.URL.Path)
-	log.Printf("Received request for path: %s", r.URL.Path)
-}
+const (
+	CONN_HOST = "0.0.0.0"
+	CONN_PORT = "8180"
+	CONN_TYPE = "tcp"
+)
 
 func main() {
-	var addr string = ":8180"
-	handler := http.HandlerFunc(HelloServer)
-	log.Printf("Starting webserver on %s", addr)
-	if err := http.ListenAndServe(addr, handler); err != nil {
-		log.Fatalf("Could not listen on port %s %v", addr, err)
+	// Listen for incoming connections.
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		log.Printf("Error listening: %s", err.Error())
+		return
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	log.Printf("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Printf("Error accepting: %s", err.Error())
+			return
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
 	}
 }
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	_, err := conn.Read(buf)
+	log.Printf("received: %s", string(buf[:]))
+	if err != nil {
+		log.Printf("Error reading: %s", err.Error())
+	}
+	// Send a response back to person contacting us.
+	conn.Write([]byte("Message received."))
+	// Close the connection when you're done with it.
+	conn.Close()
+}
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// )
+
+// // HelloServer responds to requests with the given URL path.
+// func HelloServer(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello, you requested: %s", r.URL.Path)
+// 	log.Printf("Received request for path: %s", r.URL.Path)
+// }
+
+// func main() {
+// 	var addr string = ":8180"
+// 	handler := http.HandlerFunc(HelloServer)
+// 	log.Printf("Starting webserver on %s", addr)
+// 	if err := http.ListenAndServe(addr, handler); err != nil {
+// 		log.Fatalf("Could not listen on port %s %v", addr, err)
+// 	}
+// }
